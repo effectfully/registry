59e1dc20b83a61a9fc982eb5435331eef23ea1ed
effectfully <effectfully@gmail.com>
2022-04-10 20:14:57 +0200

    Init

---
+++ src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib
+    ( module Export
+    ) where
+
+import Prelude as Export
+
+import Control.Monad.Trans as Export
+import Control.Monad.Trans.Class as Export
+import Data.ByteString as Export
+import Data.Text as Export
