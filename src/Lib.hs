71cab54055f15f066988b0c91a17d1639374f762
effectfully <effectfully@gmail.com>
2023-05-20 22:59:22 +0200

    Bump LTS, remove TAGS, add a bunch of libraries

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,12 @@ module Lib
     ( module Export
     ) where
 
-import Prelude as Export
+import Prelude as Export ()
 
-import Control.Monad.Trans as Export
-import Control.Monad.Trans.Class as Export
-import Data.ByteString as Export
-import Data.Text as Export
+import Control.Monad.Trans as Export ()
+import Control.Monad.Trans.Class as Export ()
+import Data.ByteString as Export ()
+import Data.Text as Export ()
+import Test.QuickCheck as Export ()
+import Test.Tasty as Export ()
+import Data.Vector as Export ()
